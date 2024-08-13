885d38d36686fb14de8995f136b996f86bdf7483
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-08-13 17:51:14 -0400

    fix: Set all variables on all directories. (#50)

--- docker/setup-run-server.sh
+++ docker/setup-run-server.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
+# Pentaho credentials
 sed -i "s|@CARTE_PASSWORD@|$CARTE_PASSWORD|g" /opt/pentaho/server.xml
 sed -i "s|@CRON_SETUP@|$CRON_SETUP|g" /opt/pentaho/setup_crontab.sh
 
+
 # Email config
-sed -i "s|@EMAIL_SMTP_SERVER@|$EMAIL_SMTP_SERVER|g" /opt/pentaho/repo/Job/*.kjb
-sed -i "s|@EMAIL_SMTP_PORT@|$EMAIL_SMTP_PORT|g" /opt/pentaho/repo/Job/*.kjb
-sed -i "s|@EMAIL_SENDER_ADDRESS@|$EMAIL_SENDER_ADDRESS|g" /opt/pentaho/repo/Job/*.kjb
-sed -i "s|@EMAIL_PASSWORD@|$EMAIL_PASSWORD|g" /opt/pentaho/repo/Job/*.kjb
-sed -i "s|@EMAIL_SENDER_NAME@|$EMAIL_SENDER_NAME|g" /opt/pentaho/repo/Job/*.kjb
-sed -i "s|@EMAIL_DESTINATION_ADDRESS@|$EMAIL_DESTINATION_ADDRESS|g" /opt/pentaho/repo/Job/*.kjb
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@EMAIL_SMTP_SERVER@|$EMAIL_SMTP_SERVER|g" {} \;
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@EMAIL_SMTP_PORT@|$EMAIL_SMTP_PORT|g" {} \;
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@EMAIL_SENDER_ADDRESS@|$EMAIL_SENDER_ADDRESS|g" {} \;
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@EMAIL_PASSWORD@|$EMAIL_PASSWORD|g" {} \;
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@EMAIL_SENDER_NAME@|$EMAIL_SENDER_NAME|g" {} \;
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@EMAIL_DESTINATION_ADDRESS@|$EMAIL_DESTINATION_ADDRESS|g" {} \;
+
+# sed -i "s|@EMAIL_SMTP_SERVER@|$EMAIL_SMTP_SERVER|g" /opt/pentaho/repo/Job/*.kjb
+# sed -i "s|@EMAIL_SMTP_PORT@|$EMAIL_SMTP_PORT|g" /opt/pentaho/repo/Job/*.kjb
+# sed -i "s|@EMAIL_SENDER_ADDRESS@|$EMAIL_SENDER_ADDRESS|g" /opt/pentaho/repo/Job/*.kjb
+# sed -i "s|@EMAIL_PASSWORD@|$EMAIL_PASSWORD|g" /opt/pentaho/repo/Job/*.kjb
+# sed -i "s|@EMAIL_SENDER_NAME@|$EMAIL_SENDER_NAME|g" /opt/pentaho/repo/Job/*.kjb
+# sed -i "s|@EMAIL_DESTINATION_ADDRESS@|$EMAIL_DESTINATION_ADDRESS|g" /opt/pentaho/repo/Job/*.kjb
 
 
 # ADempiere Backend connection
-sed -i "s|@TOKEN@|$TOKEN|g" /opt/pentaho/repo/Job/*.kjb
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@TOKEN@|$TOKEN|g" {} \;
+
+# sed -i "s|@TOKEN@|$TOKEN|g" /opt/pentaho/repo/Job/*.kjb
+
 
 # Play Backend
-sed -i "s|@PLAY_HOST_URL@|$PLAY_HOST_URL|g" /opt/pentaho/repo/Job/*.kjb
-sed -i "s|@PLAY_OUTPUT_PATH@|$PLAY_OUTPUT_PATH|g" /opt/pentaho/repo/Job/*.kjb
-sed -i "s|@PLAY_INPUT_PATH@|$PLAY_INPUT_PATH|g" /opt/pentaho/repo/Job/*.kjb
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@PLAY_HOST_URL@|$PLAY_HOST_URL|g" {} \;
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@PLAY_OUTPUT_PATH@|$PLAY_OUTPUT_PATH|g" {} \;
+find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@PLAY_INPUT_PATH@|$PLAY_INPUT_PATH|g" {} \;
+
+# sed -i "s|@PLAY_HOST_URL@|$PLAY_HOST_URL|g" /opt/pentaho/repo/Job/*.kjb
+# sed -i "s|@PLAY_OUTPUT_PATH@|$PLAY_OUTPUT_PATH|g" /opt/pentaho/repo/Job/*.kjb
+# sed -i "s|@PLAY_INPUT_PATH@|$PLAY_INPUT_PATH|g" /opt/pentaho/repo/Job/*.kjb
+
 
 # Journal Backend
 find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@JOURNAL_SOURCE_DB_HOST@|$JOURNAL_SOURCE_DB_HOST|g" {} \;
@@ -36,5 +53,5 @@ find /opt/pentaho/repo -type f -name "*.kjb" -exec sed -i "s|@JOURNAL_HOST_URL@|
 # sed -i "s|@JOURNAL_HOST_URL@|$JOURNAL_HOST_URL|g" /opt/pentaho/repo/Job/*.kjb
 
 
-#sh /opt/pentaho/setup_crontab.sh
+# sh /opt/pentaho/setup_crontab.sh
 sh /opt/pentaho/run-server.sh
